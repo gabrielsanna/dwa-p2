1f603a43765083706146e91308116976713aae73
gabrielsanna <gabrielsanna1@gmail.com>
2017-09-19 18:37:33 -0400

    Added styles and functions file

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
+<?php require 'functions.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title</title>
 	<meta charset='utf-8'>
+
+	<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	
 </head>
 <body>
+	<div id="wrapper">
 
-	<h1>Project 2</h1>
+		<h1>Project 2</h1>
 
+		<select>
+			<option value="webserver">Type of Web Server</option>
+			<option value="ipaddress">IP Address</option>
+			<option value="setscookie">Does this page set a cookie?</option>
+			<option value="all">All Information</option>
+		</select>
+
+	</div>
 </body>
 </html>
