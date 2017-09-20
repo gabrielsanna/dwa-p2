1a230c0edf8ef678ec54e1eb23cf203282999e5f
gabrielsanna <gabrielsanna1@gmail.com>
2017-09-19 21:23:22 -0400

    Add bootstrap stylesand DNS lookup

--- functions.php
+++ functions.php
@@ -2,5 +2,14 @@
 
 require 'helpers.php';
 
+var_dump($_GET);
+
+$siteUrl = $_GET['searchUrl'];
+
+# Query the IP address
+$dnsRecord = dns_get_record($siteUrl, DNS_A);
+$ipAddress = $dnsRecord[0]['ip'];
+
+# Request an HTTP header and store as an array
 
 ?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<title</title>
 	<meta charset='utf-8'>
 
-	<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="styles.css">
 
 </head>
@@ -16,35 +16,48 @@
 
 		<h1>Project 2</h1>
 
-		<form method='GET' action='search.php'>
+		<form method='GET'>
 
-    		<label>Enter a web URL:
-        		<input type='text' name='searchTerm'>
-    		</label>
-    		<br>
+			<div class="form-group">
+	    		<label>Enter a web URL:</label>
+	        	<input type='text' class="form-control" type="url" name='searchUrl' placeholder="www.example.com">
+    		</div>
 
-    		<label>What information would you like to query?
-	    		<select>
+    		<div class="form-group">
+    			<label for='querySelect'>What information would you like to query?</label>
+		    	<select class="form-control" id="querySelect">
 					<option value="webserver">Type of Web Server</option>
 					<option value="ipaddress">IP Address</option>
 					<option value="setscookie">Does this page set a cookie?</option>
 					<option value="all">All Information</option>
 				</select>
-			</label>
-			<br>
+			</div>
 
-    		<input type='submit' value='Search'>
+			<fieldset class="form-group">
+				<label class="form-check-label">What protocol should be used?</label>
+				<div class="form-check">
+					<label class="form-check-label">
+						<input type="radio" class="form-check-input" name="protocol" value="http" checked> HTTP
+					</label>
+				</div>
+				<div class="form-check">
+					<label class="form-check-label">
+						<input type="radio" class="form-check-input" name="protocol" value="https"> HTTPS
+					</label>
+				</div>
+			</fieldset>
+
+    		<input type='submit' class='btn btn-primary btn-small' value='Search'>
 
 		</form>
 
-
-
-			<div class="data-output">
-				<h2>URL</h2>
-				<h3>IP Address: x</h3>
-				<h3>Web server software: x</h3>
-				<h3>Does page set a cookie? x</h3>
-			</div>
+		<br>
+		<div class="data-output">
+			<h2><?=$siteUrl?></h2>
+			<h3>IP Address: <?=$ipAddress?></h3>
+			<h3>Web server software: x</h3>
+			<h3>Does page set a cookie? x</h3>
+		</div>
 
 	</div>
 </body>
