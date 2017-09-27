2544faed6d1f5ce798199b8f71dbf9946e90ad63
gabrielsanna <gabrielsanna1@gmail.com>
2017-09-26 20:55:53 -0400

    Bug fixes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
 
-	<title</title>
+	<title>Project 2</title>
 	<meta charset='utf-8'>
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
